acdbc757a95a88909b9d31baa8881f4b2dc8687f
ocecconi <ocecconi@gmail.com>
2013-07-17 22:54:40 -0400

    fixed for {} bug

--- e1.js
+++ e1.js
@@ -7,13 +7,18 @@
 max = 1000;
 sum = 0;
 
+
 var mult = function(max) {
-	for (i = 1; i < below; i++)
+	for (i = 1; i < below; i++){
 		if (i%3 == 0)
-			{sum = sum + i;}
+			{sum = sum + i;
+			}
 		else if (i%5 == 0)
-			{sum = sum + i;}
-		else {sum = sum;}
+			{sum = sum + i;
+			}
+		else {sum = sum;
+		}
+	}
 }
 
 
