26c0cd1de79e238cbb0c9f4e40d0ba300f3309da
ocecconi <ocecconi@gmail.com>
2013-07-17 22:40:02 -0400

    added

---
+++ e1.js
@@ -0,0 +1,19 @@
+// brute force approach:
+// check every number if divisible by 3 or 5
+// if it is, add the number to the rallying sum
+
+max = 1000;
+sum = 0;
+
+var mult = function(max) {
+	for (i = 1; i < below; i++)
+		if (i%3 == 0)
+			{sum = sum + i;}
+		else if (i%5 == 0)
+			{sum = sum + i;}
+		else {sum = sum;}
+}
+
+
+// output
+console.log(sum);
